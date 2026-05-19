87b21b3f70016a2ea8bbda4b772be827d52f0336
김도완 <gimruning@gmail.com>
2026-05-19 12:44:52 +0000

    add posts/ to gitignore and update comments router

--- routers/comments.py
+++ routers/comments.py
@@ -9,7 +9,7 @@ from dependencies import get_current_user
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-
+# 댓글 등록
 @router.post("/posts/{id}/comments")
 async def create_comment(id: int, content: str = Form(...), current_user=Depends(get_current_user)):
 	if not current_user:
@@ -18,8 +18,9 @@ async def create_comment(id: int, content: str = Form(...), current_user=Depends
 	models.create_comments(id, username, content)
 	return RedirectResponse(url=f"/posts/{id}", status_code=303)
 
-
+# 댓글 수정
 @router.post('/comments/{id}/edit')
+#id 받기
 async def edit_comment(id: int, content: str = Form(...), current_user=Depends(get_current_user)):
 	if not current_user:
 		return RedirectResponse(url=f'/posts/{id}', status_code=303)
@@ -31,7 +32,7 @@ async def edit_comment(id: int, content: str = Form(...), current_user=Depends(g
 	comment = models.get_comment(id)
 	return RedirectResponse(url=f'/posts/{comment["post_id"]}', status_code=303)
 
-
+# 댓글 삭제
 @router.post('/comments/{id}/delete')
 async def delete_comment(id: int, current_user=Depends(get_current_user)):
 	if not current_user:
