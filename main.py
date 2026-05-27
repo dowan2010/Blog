12bebbaa8e87c6970451f6b4b7cf21939fb1977c
김도완 <gimruning@gmail.com>
2026-05-27 19:50:33 +0900

    커밋

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from contextlib import asynccontextmanager
@@ -18,3 +19,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(posts.router)
 app.include_router(comments.router)
 app.include_router(auth.router)
+
+if __name__ == "__main__":
+    # 스크립트를 직접 실행할 때만 서버 구동
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
